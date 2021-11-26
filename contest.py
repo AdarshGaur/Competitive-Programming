ca93e39024008c6e3b905743689426121f510ede
Adarsh Gaur <adarshgaur3@gmail.com>
2021-11-26 21:25:06 +0530

    added script

---
+++ contest.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import os, shutil
+
+name = input("Contest Name - ")
+contest_name = name.replace(" ", "-") + "/"
+
+par_dir = os.getcwd();
+contest_dir = os.path.join(par_dir, contest_name)
+
+try :
+	os.mkdir(contest_dir)
+	# print("success")
+except OSError as e:
+	print(e);
+	exit()
+
+# template = par_dir + "template.cpp"
+files = ('a', 'b', 'c', 'd', 'e')
+src = par_dir+"/template.cpp"
+
+for i in files:
+	dest = contest_dir + i + ".cpp"
+	copied_path = shutil.copy(src, dest)
+	# print("success")
+
+# END OF FILE
