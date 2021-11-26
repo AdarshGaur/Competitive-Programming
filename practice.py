f68b3db4377c02d76eb58b059424957c8e0036a0
Adarsh Gaur <adarshgaur3@gmail.com>
2021-11-26 23:26:50 +0530

    modified practice script a lil too

--- practice.py
+++ practice.py
@@ -9,11 +9,11 @@ par_dir = os.getcwd();
 src = par_dir + "/template.cpp"
 par_dir += "/practice-problems"
 problem_dir = os.path.join(par_dir, problem_name)
-# print(problem_dir)
 dest = problem_dir
+
 try:
 	copied_path = shutil.copy(src, dest);
-	print("success")
+	# print("success")
 except IOError as e:
 	print("Error: ", e)
 	print("Try with some addons :/")
