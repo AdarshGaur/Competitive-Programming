0f5d49994782b039edab03665fa76139cce838f5
Adarsh Gaur <adarshgaur3@gmail.com>
2022-03-06 20:41:55 +0530

    added Round 752

---
+++ Codeforces/Codeforces Round #752 (Div. 2)/D. Moderate Modular Mode.cpp
@@ -0,0 +1,46 @@
+// Author : Adarsh Gaur 
+
+// #define NDEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+
+#define endl "\n"
+#define aster(x) cerr<<#x<<" = "<<x<<endl;
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+
+void solve(){
+	int x, y;
+	cin >> x >> y;
+	int n;
+	if( x > y){
+		n = x + y;
+	}else if(x==y){
+		n = x;
+	}else{
+		int k = (y/x) * x;
+		n = k + (y-k)/2;
+	}
+	cout << n << endl;		
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
