6ab851c8e8975e07dad14243f3243b31c03ba9d7
Adarsh Gaur <adarshgaur3@gmail.com>
2022-03-29 20:29:05 +0530

    added Debug functions

--- Template.cpp
+++ Template.cpp
@@ -7,19 +7,44 @@ using namespace std;
 typedef pair<int,int> pii;
 typedef vector<int> vi;
 
-#define endl "\n"
-#define aster(x) cerr<<#x<<" = "<<x<<endl;
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef cicada
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) "Throughout the hells, I alone am the honored one."
+#endif
 
 int const INF = 2e18;
 int const mod = 1e9 +7;
 // int const mod = 998244353;
 // int const N = 2e5 + 7;
 
+
 void solve(){
 	
 }
 
-int32_t main()
+signed main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);cout.precision(20);
