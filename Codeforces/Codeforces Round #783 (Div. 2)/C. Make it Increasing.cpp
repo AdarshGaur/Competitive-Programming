c0a59c5693aa0e11053284f082d4ac43488b31d1
Adarsh Gaur <adarshgaur3@gmail.com>
2022-04-21 15:07:44 +0530

    added Round 783

---
+++ Codeforces/Codeforces Round #783 (Div. 2)/C. Make it Increasing.cpp
@@ -0,0 +1,85 @@
+// https://github.com/AdarshGaur/Competitive-Programming
+// Author : @AdarshGaur
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+
+
+// Source : https://codeforces.com/blog/entry/76087
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef cicada
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) "Throughout the hells, I alone am the honored one."
+#endif
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+
+void solve(){
+	int n;
+	cin >> n;
+	int a[n];
+	for(int &i: a) cin >> i;
+	int ans = INF;
+	for(int i=0; i<n; i++){
+		vi aux(n, 0);
+		int cnt = 0;
+		for(int j=i+1; j<n; j++){
+			int x  = (aux[j-1] / a[j]);
+			if(x * a[j] <= aux[j-1]) x++;
+			aux[j] = x*a[j];
+			cnt += x;
+		}
+		for(int j=i-1; j>=0; j--){
+			int x = aux[j+1]/a[j];
+			if(x*a[j] <= aux[j+1]) x++;
+			aux[j] = x*a[j];
+			cnt += x;
+		}
+		ans = min(ans, cnt);
+	}
+	cout << ans << endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	// cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
