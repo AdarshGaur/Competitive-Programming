4bf5b64e3e902a826bc84ef042288640752b77d9
Adarsh Gaur <adarshgaur3@gmail.com>
2022-05-25 23:20:16 +0530

    added Edu Round 125

---
+++ Codeforces/Educational Codeforces Round 125 (Rated for Div. 2)/B. XY Sequence.cpp
@@ -0,0 +1,41 @@
+// Author : Adarsh Gaur 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+
+#define endl "\n"
+#define aster(x) cerr<<#x<<" = "<<x<<endl;
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+void solve(){
+	int n, b, x, y;
+	cin >> n >> b >> x >> y;
+	int ans = 0, last = 0;
+	for(int i=0; i<n; i++){
+		if(last + x <= b) last += x;
+		else last -= y;
+		ans += last;
+	}
+	cout << ans << endl;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
---
+++ Codeforces/Educational Codeforces Round 125 (Rated for Div. 2)/C. Bracket Sequence Deletion.cpp
@@ -0,0 +1,50 @@
+// Author : Adarsh Gaur 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+
+#define endl "\n"
+#define aster(x) cerr<<#x<<" = "<<x<<endl;
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+void solve(){
+	string s;
+	int n;
+	cin >> n >> s;
+	int op = 0, i = 0;
+	while(i < n-1){
+		if(s[i] == '(' or s[i+1] == ')'){
+			i+=2;
+		}else{
+			int j = i+2;
+			while(j<n and s[j] == '('){
+				j++;
+			}
+			if(j >= n ) break;
+			i = j+1;
+		}
+		op++;
+	}
+	cout << op << " " << n - i << endl;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
