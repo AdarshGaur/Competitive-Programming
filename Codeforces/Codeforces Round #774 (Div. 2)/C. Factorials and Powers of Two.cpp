3bd83f9ccc64b30a2cfe44c7e3f933f59d667c92
Adarsh Gaur <adarshgaur3@gmail.com>
2022-05-27 21:42:25 +0530

    added Round 774

---
+++ Codeforces/Codeforces Round #774 (Div. 2)/C. Factorials and Powers of Two.cpp
@@ -0,0 +1,93 @@
+// Author : @AdarshGaur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+
+
+// Source : https://codeforces.com/blog/entry/76087
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef cicada
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) "Hard work betrays none but dreams betray many."
+#endif
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+
+vector<int> v;
+
+void solve(){
+	int n;
+	cin >> n;
+	auto M = lower_bound(v.begin(), v.end(), n) - v.begin();
+	if(v[M] == n){
+		cout << 1 << endl;
+		return;
+	}
+	int ans = INF;
+	for(int i=0; i <= (1<<M); i++){
+		int cnt = 0, s = 0;
+		for(int bit = 0; bit<=M; bit++){
+			if( i & (1<<bit)){
+				s += v[bit];
+				cnt++;
+			}
+		}
+		if(s > n) continue;
+		int x = n - s;
+		while(x){
+			if(x &1)
+				cnt++;
+			x >>= 1;
+		}
+		ans = min(ans, cnt);
+	}
+	cout << ans << endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	//////////////
+	int fact = 2;
+	for(int i=3; i<16; i++){
+		fact *= i;
+		v.push_back(fact);
+	}
+	//////////////
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
