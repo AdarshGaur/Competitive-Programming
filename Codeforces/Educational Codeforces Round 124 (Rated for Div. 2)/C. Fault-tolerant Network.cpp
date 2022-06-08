36bcaad021dd195e8a547a13399eaba9e8ff0cfb
Adarsh Gaur <adarshgaur3@gmail.com>
2022-06-08 21:13:45 +0530

    added Edu Round 124

---
+++ Codeforces/Educational Codeforces Round 124 (Rated for Div. 2)/C. Fault-tolerant Network.cpp
@@ -0,0 +1,84 @@
+// Author : @AdarshGaur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+
+
+// Source : https://codeforces.com/blog/entry/76087
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef cicada
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) "Yume o akiramete shinde kure."
+#endif
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+int f(vector<int>& a, int val){
+	int minDiff = abs(val - a[0]);
+	for(auto i: a){
+		if(abs(val - i) < minDiff){
+			minDiff = abs(val - i);
+		}
+	}
+	return minDiff;
+}
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> a(n), b(n);
+	for(int &i: a)
+		cin >> i;
+	for(int &i: b)
+		cin >> i;
+	int ans = abs(a[0] - b[0]) + abs(a[n-1] - b[n-1]);
+	ans = min(ans, abs(a[n-1]-b[0]) + abs(a[0] - b[n-1]));
+	ans = min(ans, abs(a[0]-b[0]) + f(a, b[n-1]) + f(b, a[n-1]));
+	ans = min(ans, abs(a[0]-b[n-1]) + f(a, b[0]) + f(b, a[n-1]));
+	ans = min(ans, abs(a[n-1]-b[n-1]) + f(a, b[0]) + f(b, a[0]));
+	ans = min(ans, abs(a[n-1]-b[0]) + f(a, b[n-1]) + f(b, a[0]));
+	ans = min(ans, f(a, b[0]) + f(a, b[n-1]) + f(b, a[0]) + f(b, a[n-1]));
+	cout << ans << endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
