e20bedd5908c66c99dd1ac09237f7cb8a2e2ecaa
Adarsh Gaur <adarshgaur3@gmail.com>
2022-06-11 13:21:21 +0530

    added Round 798

---
+++ Codeforces/Codeforces Round #798 (Div. 2)/B. Mystic Permutation.cpp
@@ -0,0 +1,87 @@
+// Author : @AdarshGaur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+
+
+// Source : https://codeforces.com/blog/entry/76087
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef cicada
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) "Yume o akiramete shinde kure."
+#endif
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+
+void solve(){
+	int n;
+	cin >> n;
+	int p[n];
+	for(int &i : p)
+		cin >> i;
+	if(n == 1){
+		cout << -1 << endl;
+		return ;
+	}
+	vector<int> ans;
+	int j = 1;
+	for(int i=0; i<n-1; i++, j++){
+		if(p[i] == j){
+			ans.push_back(j+1);
+			ans.push_back(j++);
+			i++;
+		}else{
+			ans.push_back(j);
+		}
+	}
+	if((int)ans.size() != n){
+		ans.push_back(j);
+	}
+	if(ans[n-1] == p[n-1])
+		swap(ans[n-1], ans[n-2]);
+	for(int i=0; i<n; i++)
+		cout << ans[i] << " \n"[i == n-1];
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
