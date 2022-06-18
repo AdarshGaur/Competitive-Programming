8805d403d0563f88bb201bf90b884fe9dc61415a
Adarsh Gaur <adarshgaur3@gmail.com>
2022-06-18 14:03:13 +0530

    added Round 799

---
+++ Codeforces/Codeforces Round #799 (Div. 4)/F.cpp
@@ -0,0 +1,82 @@
+// Author : @AdarshGaur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+typedef pair<int,int> pii;
+
+
+// Source : https://codeforces.com/blog/entry/76087
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef cicada
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) "Yume o akiramete shinde kure."
+#endif
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+
+void solve(){
+	int n;
+	cin >> n;
+	int cnt[10] = {};
+	vector<int> a;
+	for(int i=0; i<n; i++){
+		int x; cin >> x;
+		x %= 10;
+		if(cnt[x] < 3){
+			cnt[x]++;
+			a.push_back(x);
+		}
+	}
+	for(int i=0; i<(int)a.size(); i++){
+		for(int j=i+1; j<(int)a.size(); j++){
+			for(int k=j+1; k<(int)a.size(); k++){
+				if((a[i] + a[j] + a[k])%10 == 3){
+					cout << "YES" << endl;
+					return;
+				}
+			}
+		}
+	}
+	cout << "NO" << endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
