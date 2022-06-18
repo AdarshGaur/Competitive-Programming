b2c1c3b918e119ebdf2a38819621b69d5cdac723
Adarsh Gaur <adarshgaur3@gmail.com>
2022-06-19 01:52:56 +0530

    added Round 801

---
+++ Codeforces/Codeforces Round #801 (Div. 2) and EPIC Institute of Technology Round/C. Zero Path.cpp
@@ -0,0 +1,89 @@
+// Author : @AdarshGaur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+#define endl "\n"
+typedef pair<int,int> pii;
+
+
+// Source : https://codeforces.com/blog/entry/76087
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef cicada
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) "Yume o akiramete shinde kure."
+#endif
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+// int const mod = 998244353;
+// int const N = 2e5 + 7;
+
+
+void solve(){
+	int n, m;
+	cin >> n >> m;
+	int a[n][m];
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			cin >> a[i][j];
+		}
+	}
+	if( (n+m-1)%2 != 0){
+		cout << "NO" << endl;
+		return;
+	}
+	vector<vector<int>> mn(n, vector<int>(m, INF)), mx(n, vector<int>(m, -INF));
+	mn[0][0] = mx[0][0] = a[0][0];
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			if(i>0){
+				mn[i][j] = min(mn[i][j], mn[i-1][j]+a[i][j]);
+				mx[i][j] = max(mx[i][j], mx[i-1][j]+a[i][j]);
+			}
+			if(j>0){
+				mn[i][j] = min(mn[i][j], mn[i][j-1] + a[i][j]);
+				mx[i][j] = max(mx[i][j], mx[i][j-1] + a[i][j]);
+			}
+		}
+	}
+	if(mn[n-1][m-1]<=0 and mx[n-1][m-1] >=0)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+	int test=1;
+	cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
