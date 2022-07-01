c8c35706b0e286a30662c7165fd54ec72a425f96
Adarsh Gaur <adarshgaur3@gmail.com>
2022-07-01 23:36:05 +0530

    modified Template

--- Template.cpp
+++ Template.cpp
@@ -3,40 +3,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#ifdef cicada
+#include "../../Debug.h"
+#else
+#define debug(...) "Yume o akiramete shinde kure."
+#endif
+
 #define int int64_t
 #define endl "\n"
 typedef pair<int,int> pii;
-typedef vector<int> vi;
-
-
-// Source : https://codeforces.com/blog/entry/76087
-vector<string> vec_splitter(string s) {
-	s += ',';
-	vector<string> res;
-	while(!s.empty()) {
-		res.push_back(s.substr(0, s.find(',')));
-		s = s.substr(s.find(',') + 1);
-	}
-	return res;
-}
-void debug_out(
-vector<string> __attribute__ ((unused)) args,
-__attribute__ ((unused)) int idx, 
-__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
-template <typename Head, typename... Tail>
-void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
-	if(idx > (int)0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
-	stringstream ss; ss << H;
-	cerr << args[idx] << " = " << ss.str();
-	debug_out(args, idx + 1, LINE_NUM, T...);
-}
-
-#ifdef cicada
-#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
-#else
-#define debug(...) "..."
-#endif
-
 int const INF = 2e18;
 int const mod = 1e9 +7;
 // int const mod = 998244353;
