9a9306c1d42a5f4f9f60b8ef38e1f484592de551
Adarsh Gaur <adarshgaur3@gmail.com>
2023-01-01 13:42:43 +0530

    added ABC 214

---
+++ Atcoder/AtCoder Beginner Contest 214/D - Sum of Maximum Weights.cpp
@@ -0,0 +1,100 @@
+// Author : @AdarshGaur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef cicada
+#include "debug.h"
+#else
+#define debug(...) "Yume o akiramete shinde kure."
+#endif
+
+#define int int64_t
+#define endl "\n"
+typedef pair<int,int> pii;
+
+int const INF = 2e18;
+int const mod = 1e9 +7;
+
+
+struct DSU{
+	vector<int> par, weight;
+	int components, nodes;
+
+	DSU(int n){
+		nodes = n;
+		components = n;
+		par.resize(n+1);
+		weight.resize(n+1, 1);
+		iota(par.begin(), par.end(), 0);
+	}
+
+	int Find(int v){ // find set with path compression
+		if(v==par[v])
+			return v;
+		return par[v] = Find(par[v]);
+	}
+
+	bool Union(int a, int b){
+		a = Find(a), b = Find(b);
+		if(a == b) return false;
+		if(weight[a] > weight[b]){
+			swap(a,b);
+		}
+		par[a] = b;
+		weight[b] += weight[a];
+		components -= 1;
+		return true;
+	}
+
+	int CntofComponent(){ // return the total number of components
+		return components;
+	}
+
+	int NodeinComponent(int x){ // return the number of nodes in component x;
+		return weight[Find(x)];
+	}
+};
+
+
+void solve(){
+	int n;
+	cin >> n;
+	
+	vector<tuple<int,int,int>> edges;
+	for(int i=0; i<n-1; i++){
+		int u, v, w;
+		cin >> u >> v >> w;
+		edges.push_back({w,u,v});
+	}
+	sort(begin(edges), end(edges));
+	
+	
+	DSU dsu(n);
+	int ans = 0;
+	for(auto &[w, u, v]: edges){
+		int c1 = dsu.NodeinComponent(u), c2 = dsu.NodeinComponent(v);
+		ans += (w * c1 * c2);
+		dsu.Union(u, v);
+	}
+	
+	cout << ans << endl;
+	
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.precision(20);
+
+	// freopen("input.txt","r",stdin);
+	// freopen("output.txt","w",stdout);
+
+	int test=1;
+	// cin >> test;
+	for(int t=1; t<=test; t++){
+		// cout<<"Case #"<<t<<": ";
+		solve();
+	}
+	return 0;
+}
